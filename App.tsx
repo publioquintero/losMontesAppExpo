1366a9c1346adffebd9615d123fc70ddea4299a9
Publio Quintero <publio.quintero@gmail.com>
2020-05-09 15:27:29 -0400

    statusBar

--- App.tsx
+++ App.tsx
@@ -1,17 +1,19 @@
 import * as React from "react";
 import {WebView} from "react-native-webview";
-import {StyleSheet, SafeAreaView} from "react-native";
-import { SplashScreen } from 'expo';
+import {StyleSheet, SafeAreaView, StatusBar} from "react-native";
 
 export default class App extends React.Component {
    render() {
       return (
-         <SafeAreaView style={styles.container}>
-            <WebView
-               source={{uri: "https://www.superlosmontes.com/site/"}}
-               style={{marginTop: 20}}
-            />
-         </SafeAreaView>
+         <React.Fragment>
+            <StatusBar backgroundColor="blue" barStyle="dark-content" />
+            <SafeAreaView style={styles.container}>
+               <WebView
+                  source={{uri: "https://www.superlosmontes.com/site/"}}
+                  style={{marginTop: 20}}
+               />
+            </SafeAreaView>
+         </React.Fragment>
       );
    }
 }
@@ -19,5 +21,6 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
    container: {
       flex: 1,
+      height: 10,
    },
 });
